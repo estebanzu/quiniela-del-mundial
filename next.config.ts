3cca17c9433328ac41b152432c9122f569345afc
ezunigam <esteban.zunigamora@windriver.com>
2026-06-24 08:17:13 -0600

    chore: add dev IP to allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.100.186', '127.0.0.1'],
+  allowedDevOrigins: ['192.168.100.186', '192.168.100.213', '127.0.0.1'],
 };
 
 export default nextConfig;
